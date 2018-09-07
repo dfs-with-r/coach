c4bc59ea5b433d8bf9ab7d6a916b1ac018384df4
Robert Zamora <zamora.rr@gmail.com>
2018-09-07 09:45:58 -0400

    add fanduel reader

---
+++ R/read-fd.r
@@ -0,0 +1,32 @@
+#' Read Fanduel Players
+#'
+#' Expects a csv from \url{https://www.fanduel.com/} obtained by clicking
+#' "Download Players List" for your contest page.
+#'
+#' @param path path to csv file
+#' @export
+read_fd <- function(path) {
+  df <- read.csv(path, stringsAsFactors = FALSE)
+
+  # keep only first columns
+  df <- df[1:12]
+
+  # extract location
+  locations <- parse_locations(df[["Game"]])
+  df[["location"]] <- locations[,3]
+
+  # select columns
+  df_tidy <- df[c("Id", "Nickname", "Team", "Opponent", "location", "Position",
+                  "Salary", "FPPG", "Injury.Indicator")]
+  colnames(df_tidy) <- c("player_id", "player", "team", "opp_team", "location",
+                         "position", "salary", "fpts_avg", "injury")
+
+  # fix injury NAs
+  df_tidy[["injury"]] <- with(df_tidy, ifelse(nchar(injury) == 0, NA_character_, injury))
+
+  # add row ids
+  df_tidy <- add_row_id(df_tidy)
+
+  # tibble
+  tibble::as_tibble(df_tidy)
+}
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -14,3 +14,25 @@ test_that("draftkings mlb reader works", {
   expect_equal(ncol(df), 9)
 })
 
+test_that("fanduel reader works", {
+  nfl <- read_fd("data/fd-nfl.csv")
+  expect_equal(nrow(nfl), 715)
+  expect_equal(ncol(nfl), 10)
+
+  mlb <- read_fd("data/fd-mlb.csv")
+  expect_equal(nrow(mlb), 806)
+  expect_equal(ncol(mlb), 10)
+
+  nba <- read_fd("data/fd-nba.csv")
+  expect_equal(nrow(nba), 413)
+  expect_equal(ncol(nba), 10)
+
+  nhl <- read_fd("data/fd-nhl.csv")
+  expect_equal(nrow(nhl), 581)
+  expect_equal(ncol(nhl), 10)
+
+  pga <- read_fd("data/fd-pga.csv")
+  expect_equal(nrow(pga), 156)
+  expect_equal(ncol(pga), 10)
+})
+
