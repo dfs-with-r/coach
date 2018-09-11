8787aef5ebdf610814b0d3fec45728df250ecf12
Robert Zamora <zamora.rr@gmail.com>
2018-09-11 15:06:06 -0400

    normalize positions for various sites and sports

--- R/write.r
+++ R/write.r
@@ -41,3 +41,130 @@ normalize_positions <- function(pos, pos_max, wildcard) {
   pos_df[["new_pos"]] <- with(pos_df, ifelse(rank > max_rank, wildcard, pos))
   pos_df[["new_pos"]]
 }
+
+#' @rdname normalize_positions
+#' @keywords internal
+normalize_dk_nfl <- function(pos) {
+  stopifnot(length(pos) == 9L)
+
+  pos_max <- c("QB" = 1, "RB" = 2, "WR" = 3, "TE" = 1, "DST" = 1)
+  wildcard <- "FLEX"
+  normalize_positions(pos, pos_max, wildcard)
+}
+
+#' @rdname normalize_positions
+#' @keywords internal
+normalize_dk_nba <- function(pos) {
+  stopifnot(length(pos) == 8L)
+
+  # normalize guards
+  new_pos <- normalize_positions(pos, c("PG" = 1, "SG" = 1), "G")
+
+  # normalize forwards
+  new_pos <- normalize_positions(new_pos, c("SF" = 1, "PF" = 1), "F")
+
+  # normalize util
+  pos_max <- c("PG" = 1, "SG" = 1, "SF" = 1, "PF" = 1, "C" = 1, "G" = 1, "F" = 1)
+  wildcard <- "UTIL"
+  normalize_positions(new_pos, pos_max, wildcard)
+}
+
+#' @rdname normalize_positions
+#' @keywords internal
+normalize_dk_nhl <- function(pos) {
+  stopifnot(length(pos) == 9L)
+
+  # normalize wings
+  new_pos <- gsub("RW|LW|W", "W", pos)
+
+  # noramlize util
+  pos_max <- c("C" = 2, "W" = 3, "D" = 2, "G" = 1)
+  wildcard <- "UTIL"
+  normalize_positions(new_pos, pos_max, wildcard)
+}
+
+#' @rdname normalize_positions
+#' @keywords internal
+normalize_fd_nfl <- function(pos) {
+  stopifnot(length(pos) == 9L)
+
+  pos_max <- c("QB" = 1, "RB" = 2, "WR" = 3, "TE" = 1, "D" = 1)
+  wildcard <- "FLEX"
+  normalize_positions(pos, pos_max, wildcard)
+}
+
+#' @rdname normalize_positions
+#' @keywords internal
+normalize_fd_mlb <- function(pos) {
+  stopifnot(length(pos) == 9L)
+
+  # normalize C/1B
+  new_pos <- gsub("C|1B", "C/1B", pos)
+
+  # normalize util
+  pos_max <- c("P" = 1, "C/1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "OF" = 3)
+  wildcard <- "UTIL"
+  normalize_positions(new_pos, pos_max, wildcard)
+}
+
+#' Normalize a lineup based on position
+#'
+#' Applies a UTIL or FLEX tag to appropriate lineup positions
+#'
+#' @param lineup a data frame lineup
+#' @param site string
+#' @param sport string
+#' @param colname default column to apply normalization to
+#' @export
+normalize_lineup <- function(lineup, site = c("draftkings", "fanduel"),
+                             sport = c("nfl", "mlb", "nba", "nhl"),
+                             colname = "position") {
+  site <- match.arg(site)
+  sport <- match.arg(sport)
+
+  # choose normalization function
+  if (site == "draftkings") {
+    if (sport == "nfl") {
+      f <- normalize_dk_nfl
+      pos_levels <- c("QB", "RB", "WR", "TE", "FLEX", "DST")
+    }
+    else if (sport == "mlb") {
+      f <- NULL
+      pos_levels <- c("P", "C", "1B", "2B", "3B", "SS", "OF")
+    }
+    else if (sport == "nba") {
+      f <- normalize_dk_nba
+      pos_levels <- c("PG", "SG", "SF", "PF", "C", "G", "F", "UTIL")
+    }
+    else if (sport == "nhl") {
+      f <- normalize_dk_nhl
+      pos_levels <- c("C", "W", "D", "G")
+    }
+  } else if (site == "fanduel") {
+    if (sport == "nfl") {
+      f <- normalize_fd_nfl
+      pos_levels <- c("QB", "RB", "WR", "TE", "FLEX", "D")
+    }
+    else if (sport == "mlb") {
+      f <- normalize_fd_mlb
+      pos_levels <- c("P", "C/1B", "1B", "2B", "3B", "SS", "OF", "UTIL")
+    }
+    else if(sport == "nba") {
+      f <- NULL
+      pos_levels <- c("PG", "SG", "SF", "PF", "C")
+    }
+    else if (sport == "nhl") {
+      f <- NULL
+      pos_levels <- c("C", "W", "D", "G")
+    }
+  }
+
+  # apply normalization
+  if (!is.null(f)) {
+    lineup[[colname]] <- f(lineup[[colname]])
+  }
+
+  # order by position
+  pos2 <- factor(lineup[[colname]], levels = pos_levels)
+  lineup[order(pos2),]
+}
--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -19,3 +19,45 @@ test_that("position normalization works when not all positions provide a max", {
   expected <- c("PG", "G", "SG", "SF", "PF", "PF", "PF", "C")
   expect_equal(new_pos, expected)
 })
+
+test_that("dk nfl position normalization works", {
+  pos <- c("QB", "RB", "RB", "RB", "WR", "WR", "WR", "TE", "DST")
+  actual <- normalize_dk_nfl(pos)
+
+  expected <- c("QB", "RB", "RB", "FLEX", "WR", "WR", "WR", "TE", "DST")
+  expect_equal(actual, expected)
+})
+
+test_that("dk nba position normalization works", {
+  pos <- c("PG", "PG", "SG", "SF", "PF", "PF", "PF", "C")
+  actual <- normalize_dk_nba(pos)
+
+  expected <- c("PG", "G", "SG", "SF", "PF", "F", "UTIL", "C")
+  expect_equal(actual, expected)
+})
+
+test_that("dk nhl position normalization works", {
+  pos <- c("C", "C", "LW", "RW", "RW", "D", "D", "C", "G")
+  actual <- normalize_dk_nhl(pos)
+
+  expected <- c("C", "C", "W", "W", "W", "D", "D", "UTIL", "G")
+  expect_equal(actual, expected)
+})
+
+test_that("fd nfl position normalization works", {
+  pos <- c("QB", "RB", "RB", "RB", "WR", "WR", "WR", "TE", "DE")
+  actual <- normalize_fd_nfl(pos)
+
+  expected <- c("QB", "RB", "RB", "FLEX", "WR", "WR", "WR", "TE", "DE")
+  expect_equal(actual, expected)
+})
+
+test_that("fd mlb position normalization works", {
+  pos <- c("P", "1B", "2B", "2B", "3B", "SS", "OF", "OF", "OF")
+  actual <- normalize_fd_mlb(pos)
+
+  expected <- c("P", "C/1B", "2B", "UTIL", "3B", "SS", "OF", "OF", "OF")
+  expect_equal(actual, expected)
+})
+
+
